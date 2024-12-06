919a65b69bc1321f8477e86267527054b0c26cdd
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-06 14:31:24 -0800

    adjusted home base for login

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,26 @@
-import Nav from "../components/Nav";
+import Nav from "../components/Nav"
 export default function Home() {
 
   return (
     <>
       <Nav />
-      <div className="flex flex-col justify-evenly items-center w-screen h-screen">
-        <h1 className="text-3xl">Hello there. Let's get to work</h1>
+      <div className="flex flex-col justify-evenly items-center w-full h-full space-y-2 ">
+        <h1 className="text-3xl">Welcome to Quizler</h1>
+
+        <div className="Input-home flex flex-col space-y-2">
+          <input className="rounded-xl" type="text" />
+          <input className="rounded-xl" type="text" />
+          <input className="rounded-xl" type="text" />
+
+          <div>
+            <button className="rounded-2xl bg-amber-300 text-white p-2">
+              Create an Account
+            </button>
+            <button>
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     </>)
 }
