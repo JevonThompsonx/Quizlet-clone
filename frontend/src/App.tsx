221db79b825ec4d60e8e4fcb082be9a09391bb3e
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-16 19:45:58 -0500

    created separate var for ContextValues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
-import { useState, createContext } from "react"
+import { useState, createContext, useRef } from "react"
 
-export const Context = createContext<any>(null)
+export const Context = createContext<ContextType | null>(null)
 export default function App() {
-
-  const [loggedIn, SetLoggedIn] = useState<boolean>(false)
+  const email_input = useRef<HTMLInputElement>(null)
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+  const [loggingIn, setLoggingIn] = useState<boolean>(false)
+  const ContextValue: ContextType = { loggedIn, setLoggedIn, email_input, loggingIn, setLoggingIn }
   return (
     <BrowserRouter>
-      <Context.Provider value={{ loggedIn, SetLoggedIn }}>
+      <Context.Provider value={ContextValue}>
         <div className="quizler-background">
           <Routes>
             <Route path="/" element={<Home />}>
