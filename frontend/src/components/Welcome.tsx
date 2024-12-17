a3113c9ab8005ef950d5da7e6e95af3204f3b777
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-16 19:45:18 -0500

    Create Welcome.tsx

---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,31 @@
+import { Context } from "../App.tsx"
+import { useContext } from "react"
+
+const Welcome = () => {
+  //@ts-ignore
+  const { email_input } = useContext(Context)
+  return (
+    <form className="flex flex-col justify-center items-center w-full h-full" >
+      <div className="Quizler-home flex flex-col justify-evenly items-center p-6 space-y-4">
+        <h1 className="text-3xl">Welcome to Quizler</h1>
+        <h3>A simple study tool</h3>
+
+        <div className="Input-home flex flex-col space-y-2">
+          <div className="email-div flex flex-row justify-evenly items-center space-x-2">
+            <label htmlFor="email-input">email: </label>
+            <input id="email-input" className="rounded-xl p-2" type="text" ref={email_input} />
+          </div>
+          <div className="flex flex-row justify-center items-center space-x-2">
+            <button className="rounded-2xl bg-amber-300 text-white p-2">
+              Sign up
+            </button>
+            <button className="rounded-2xl bg-amber-300 text-white p-2">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>)
+}
+
+export default Welcome
