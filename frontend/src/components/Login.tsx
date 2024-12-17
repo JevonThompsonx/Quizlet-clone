c998ea8306b34dd56bdfb87f435b4dc436828683
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-16 20:47:03 -0500

    added a flex box

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,18 @@
+import { Context } from "../App.tsx"
+import { useContext } from 'react'
+import { Nav } from "../components/index.tsx"
 const Login = () => {
+  const { setWelcomePage, welcomePage, email_input } = useContext(Context)
+  if (welcomePage == true && setWelcomePage) {
+    setWelcomePage(false)
+  }
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      Welcome to the login page
-    </div>
+    <>
+      <Nav />
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        Welcome to the login page
+      </div>
+    </>
   )
 }
 
