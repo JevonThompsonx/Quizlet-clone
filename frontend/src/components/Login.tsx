e2b1dba9c1c633773e9fb641cbdf99c8d3a2c9fa
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-29 17:35:10 -0500

    Skeleton for login page created

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -9,9 +9,29 @@ const Login = () => {
   return (
     <>
       <Nav />
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        Welcome to the login page
-      </div>
+      <main className="login-home flex flex-col justify-evenly items-center h-full w-full">
+
+        <div className="flex flex-col justify-evenly items-center" >
+          <h1>Login to Quizler</h1>
+          <p>Welcome back! Please enter your credentials.</p>
+        </div>
+        <form>
+          <div >
+            <label htmlFor="email" />
+            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+          </div>
+          <div >
+            <label htmlFor="password"></label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required />
+          </div>
+          <button className="p-2 bg-blue-400 rounded-md border-black border-2" type="submit" >Login</button>
+        </form>
+        <div className="flex flex-col justify-evenly items-center space-y-2" >
+          <p>Don't have an account? <a className=" ml-2 p-1 bg-blue-400 rounded-md border-black border-2" href=" /sign_up">Sign up</a></p>
+          <p><a className="p-1 bg-blue-400 rounded-md border-black border-2" href="/reset_password">Forgot Password?</a></p>
+        </div>
+
+      </main >
     </>
   )
 }
