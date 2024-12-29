3d4deae6312747a3295ee9f7088c0b9392fc97bb
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-29 17:33:49 -0500

    moved welcome to page index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import About from "./About"
 import Home from "./Home"
 import NotFound from "./NotFound"
+import Welcome from "./Welcome"
 
-export { About, Home, NotFound }
+export { About, Home, NotFound, Welcome }
